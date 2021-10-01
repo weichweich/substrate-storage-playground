ec79314be4fa311b9e3cf7706a804c541b11e454
weichweich <14820950+weichweich@users.noreply.github.com>
2021-10-01 08:54:36 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use std::convert::TryFrom;
 
-use frame_support::{StorageHasher, Twox128, sp_runtime::traits::BlakeTwo256};
+use codec::{Decode, Encode};
+use frame_support::{sp_runtime::traits::BlakeTwo256, StorageHasher, Twox128};
 use frame_system::{Account, AccountInfo};
 use hex::FromHex;
 use hex_literal::hex;
-use codec::{Encode, Decode};
+use sp_runtime::{
+    traits::{IdentifyAccount, Verify},
+    MultiSignature,
+};
+
+/// Alias to 512-bit hash when used in the context of a transaction signature on
+/// the chain.
+pub type Signature = MultiSignature;
+
+/// Alias to the public key used for this chain, actually a `MultiSigner`. Like
+/// the signature, this also isn't a fixed size when encoded, as different
+/// cryptos have different size public keys.
+pub type AccountPublic = <Signature as Verify>::Signer;
+
+/// Alias to the opaque account ID type for this chain, actually a
+/// `AccountId32`. This is always 32 bytes.
+pub type AccountId = <AccountPublic as IdentifyAccount>::AccountId;
+
+type SessionKey = [u8; 32];
+type QueuedKeys = Vec<(AccountId, SessionKey)>;
 
 fn storage_value_key(pallet_name: &[u8], storage_name: &[u8]) -> [u8; 32] {
     let mut final_key = [0u8; 32];
@@ -15,18 +35,27 @@ fn storage_value_key(pallet_name: &[u8], storage_name: &[u8]) -> [u8; 32] {
 
 #[derive(Debug, PartialEq, Eq, Clone, Encode, Decode)]
 struct BlockAnnouncesHandshake {
-	/// Roles of the node.
-	roles: u8,
-	/// Best block number.
-	best_number: u32,
-	/// Best block hash.
-	best_hash: sp_core::H256,
-	/// Genesis block hash.
-	genesis_hash: sp_core::H256,
+    /// Roles of the node.
+    roles: u8,
+    /// Best block number.
+    best_number: u32,
+    /// Best block hash.
+    best_hash: sp_core::H256,
+    /// Genesis block hash.
+    genesis_hash: sp_core::H256,
+}
+
+#[derive(Copy, Clone, Encode, Eq, Decode, Debug, Ord, PartialEq, PartialOrd)]
+pub enum StakingStorageVersion {
+	V1_0_0,
+	V2_0_0, // New Reward calculation, MaxCollatorCandidateStake
+	V3_0_0, // Update InflationConfig
+	V4,     // Sort TopCandidates and parachain-stakings by amount
+	V5,     // Remove SelectedCandidates, Count Candidates
 }
 
 fn main() {
-    let keys: Vec<Vec<u8>> = vec![
+    let mut keys: Vec<Vec<u8>> = vec![
         b"Block".to_owned().to_vec(),
         b"BlockHash".to_owned().to_vec(),
         b"ExtrinsicData".to_owned().to_vec(),
@@ -52,6 +81,7 @@ fn main() {
         b"Utility".to_owned().to_vec(),
         b"Account".to_owned().to_vec(),
         b"Number".to_owned().to_vec(),
+        b"Did".to_owned().to_vec(),
         b"CurrentSlot".to_owned().to_vec(),
         b"Authorities".to_owned().to_vec(),
         b"Round".to_owned().to_vec(),
@@ -73,7 +103,11 @@ fn main() {
         b"BlockWeight".to_owned().to_vec(),
         b"ExtrinsicCount".to_owned().to_vec(),
         b"Author".to_owned().to_vec(),
+        b"SessionQueuedKeys".to_owned().to_vec(),
+        b"StorageVersion".to_owned().to_vec(),
     ];
+    keys.sort();
+
     for key in keys {
         println!(
             "{:?} => {:?}",
@@ -81,54 +115,7 @@ fn main() {
             hex::encode(Twox128::hash(&key[..]))
         );
     }
-    let mut encoded1 = Vec::<u8>::from_hex(b"060000000000000002000000010000000000000014187132b48694cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account1 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded1[..]);
-    println!("Collator read: {:?}", account1);
 
-    let mut encoded1 = Vec::<u8>::from_hex(b"06000000000000000200000001000000000000001468288e5ff594cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account1 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded1[..]);
-    println!("Collator write: {:?}", account1);
-
-    let mut encoded2 = Vec::<u8>::from_hex(b"06000000000000000200000001000000000000001468288e5ff594cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account2 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded2[..]);
-    println!("Account after 172042: {:?}", account2);
-
-    let mut encoded2 = Vec::<u8>::from_hex(b"06000000000000000200000001000000000000001468288e5ff594cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account2 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded2[..]);
-    println!("Account at 172052: {:?}", account2);
-
-    let mut encoded2 = Vec::<u8>::from_hex(b"06000000000000000200000001000000000000001468288e5ff594cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account2 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded2[..]);
-    println!("Account at 172053: {:?}", account2);
-
-    let mut encoded2 = Vec::<u8>::from_hex(b"060000000000000002000000010000000000000014b8dfe90a6495cf4ace4f200000000000000000000000000000000000000000000020c65abc8ed70a00000000000000000020c65abc8ed70a00000000000000").unwrap();
-    let account2 =
-        AccountInfo::<u64, pallet_balances::AccountData<u128>>::decode(&mut &encoded2[..]);
-    println!("Account at 172054: {:?}", account2);
-
-    let mut encoded3 = Vec::<u8>::from_hex(b"16a0020000000000").unwrap();
-    let blocknum = u64::decode(&mut &encoded3[..]);
-    println!("blocknumber: {:?}", blocknum);
-
-    let mut encoded4 = Vec::<u8>::from_hex(b"08e29df39b74777495ca00cd7a316ce98c5225d7088ae924b122fe0e2e6a4b556934fe8b3e78e63ca6139669cf9fc4694e5ae7c3a2f8e9fbcacf855b4716147429").unwrap();
-    let authorities = u64::decode(&mut &encoded4[..]);
-    println!("authorities: {:?}", authorities);
-
-    let ba = BlockAnnouncesHandshake {
-        roles: 4,
-        best_number: 0,
-        best_hash: sp_core::H256::from_low_u64_be(0),
-        genesis_hash: sp_core::H256::from_low_u64_be(0),
-    };
-    let mut encode = ba.encode();
-    println!("BlockAnnouncesHandshake: {:?}", encode);
-
-    let mut encoded4 = [4, 3, 181, 42, 0, 127, 14, 119, 47, 121, 138, 119, 120, 20, 141, 4, 199, 123, 120, 239, 84, 106, 12, 7, 167, 46, 58, 246, 238, 222, 6, 117, 88, 2, 223, 93, 161, 255, 246, 253, 148, 37, 31, 87, 13, 76, 156, 223, 37, 160, 71, 93, 160, 215, 173, 53, 22, 2, 144, 218, 25, 218, 216, 249, 202, 248, 191, 49, 181];
-    let authorities = BlockAnnouncesHandshake::decode(&mut &encoded4[..]);
-    println!("BlockAnnouncesHandshake: {:?}", authorities);
+    let mut version: StakingStorageVersion = StakingStorageVersion::V4;
+    println!("version: {:?}", hex::encode(StakingStorageVersion::encode(&version)));
 }
